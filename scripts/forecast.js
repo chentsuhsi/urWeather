bc2b6b2a3bee4bdfab12bb20a23afc9d0880918d
chentsuhsi <chen.tsu.hsi@gmail.com>
2017-06-19 18:01:46 -0400

    Adding scripts.js

---
+++ scripts/forecast.js
@@ -0,0 +1,4 @@
+/**
+ * Created by David on 6/19/2017.
+ */
+
--- scripts/scritps.js
+++ scripts/scritps.js
@@ -1,9 +1,16 @@
-var corsanywhere = "https://cors-anywhere.herokuapp.com/"
-var site = "https://api.darksky.net/forecast/";
-var apiKey = "fec89db37f2fcb2d5016166a49f41e82";
+var corsanywhere = "https://cors-anywhere.herokuapp.com/";
+var site = "https://api.darksky.net/forecast/fec89db37f2fcb2d5016166a49f41e82";
 
-var myLatitute = 0;
-var myLongitute = 0;
+// example: https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=
+var googleMapsTimezoneAPI_url = "https://maps.googleapis.com/maps/api/timezone/json";
+var googleMapsTimezoneAPI_Key = "AIzaSyDjB2WPD36RiK2jUTMwrJyp_UlFMpUeqic";
+
+
+var myLatitute = 0.00;
+var myLongitute = 0.00;
+var myTimezone = "";
+var myTimezoneName = "";
+var myCity = "";
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -14,28 +21,74 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    myLatitute = position.coords.latitude;
-    myLongitute = position.coords.longitude;
+    setMyPositions(position.coords.latitude,position.coords.longitude, function(){ return true} );
+    showLocation(myLatitute, myLongitute, function(){return true});
 
-    $(".quote").html("My position is: " + "<br>Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude);
-}
+    url = corsanywhere.concat(site, "/", myLatitute + "," + myLongitute);
+    var html = url;
+    $(".forecast").html(html + "<br>");
 
-function getQuote() {
-    url = corsanywhere.concat(site, "/", apiKey, "/", myLatitute, ",", myLongitute );
     $.getJSON(
         url,
         function (data) {
-            var html = "";
-
-            html = JSON.stringify(data);
+            html += JSON.stringify(data);
             $(".forecast").html(html);
         }
     )
-};
+}
+
+function setMyPositions(lat, lng, callback) {
+    myLatitute = lat;
+    myLongitute = lng;
+    callback();
+}
+
+function showLocation(lat, lng, callback) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'latLng': latlng}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results)
+            if (results[1]) {
+                /*
+                 alert(results[0].formatted_address);
+                 alert(results[0].address_components[0].long_name) -> number;
+                 alert(results[0].address_components[1].long_name) -> street;
+                 alert(results[0].address_components[2].long_name) -> city;
+                 alert(results[0].address_components[3].long_name) -> county;
+                 alert(results[0].address_components[4].long_name) -> state;
+                 alert(results[0].address_components[5].long_name) -> country;
+                 alert(results[0].address_components[6].long_name) -> postal_code;
+                 alert(results[0].address_components[6].long_name) -> postal_code_suffix;
+                 */
+                $(".myLocation").html(
+                    "City   : " + results[0].address_components[2].long_name + "<br>" +
+                    "State  : " + results[0].address_components[4].long_name + "<br>" +
+                    "Country: " + results[0].address_components[5].long_name + "<br>"
+                );
+            } else {
+                $(".myLocation").html(
+                    "My position is: " + "<br>Latitude: " + myLatitute + ", Longitude: " + myLongitute);
+            }
+        } else {
+            $(".myLocation").html(
+                "My position is: " + "<br>Latitude: " + myLatitute + ", Longitude: " + myLongitute + "<br>" +
+                "Geocoder failed due to: " + status);
+        }
+    });
+    callback();
+}
+
+function errorFunction(){
+    alert("Geocoder failed");
+}
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+}
 
 $(document).ready(function() {
 //    $('#getQuote').on('click', getQuote);
     getLocation();
-    getQuote();
 });
+
